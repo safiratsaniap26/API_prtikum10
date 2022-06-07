01590da8766ae6f76dadb5332d86a1da9019cc92
FlutLab.io <>
2022-06-07 02:27:28 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:api/pagebody.dart';
+import 'package:api/api-manager.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SoccerApp(),
+    );
+  }
+}
+
+class SoccerApp extends StatefulWidget {
+  @override
+  _SoccerAppState createState() => _SoccerAppState();
+}
+
+class _SoccerAppState extends State<SoccerApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFAFAFA),
+      appBar: AppBar(
+        backgroundColor: Color(0xFFFAFAFA),
+        elevation: 0.0,
+        title: Text(
+          "SOCCERBOARD",
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+      ),
+      //now we have finished the api service let's call it
+      //Now befo re we create Our layout let's create our API service
+      body: FutureBuilder(
+        future: SoccerApi().getAllMatches(), //Here we will call our getData() method,
+        builder: (context, snapshot) {
+          //the future builder is very intersting to use when you work with api
+          if (snapshot.hasData) {
+            print((snapshot.data).length);
+            return PageBody(snapshot.data);
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        }, // here we will buil the app layout
+      ),
+    );
+  }
+}
+//So as we can see w got our matches data,
+// the data size depend on the date and the time so
+// you can get as many data as many matches are curetly playing
+//Now let's try to get data by seasons and leagues
